3548f2d1fff02f04d046c94d14b32a4af24b6a5d
Junichi Yamamoto <junichi11@apache.org>
2025-02-01 15:59:42 +0900

    PHP 8.4 Support: Property hooks (Part 2)
    - https://github.com/apache/netbeans/issues/
    - https://wiki.php.net/rfc#php_84
    - https://wiki.php.net/rfc/property-hooks
    
    - Fix the indexer and the model
    - Use JSON format as a signature for property hooks
    - Current format is semicolon separated but it's hard to add hooks to a field signature without JSON format
    ```json
    [
        {
            "name":"set",
            "start":3651,
            "end":3690,
            "mod":1,
            "isRef":false,
            "isAttr":false,
            "hasBody":true,
            "paramSig":"$value::0::1:1:0:0:0:0::"
        }
    ]
    ```
    
    - Add interface methods
      - `FieldElemnt.isHooked()`
      - `FieldElement.HookedFieldElemnt.isHooked()`
      - `FieldElement.HookedFieldElemnt.getPropertyHooks()`
      - `IndexScope.PHP84IndexScope.findFields()`
      - `TypeScope.FieldDeclarable.getDeclaredFields()`
      - `TypeScope.FieldDeclarable.getInheritedFields()`
      - `PropertyHookScope.isReference()`
      - `PropertyHookScope.hasBody()`
      - `PropertyHookScope.isAttributed()`
      - `PropertyHookScope.getParameterNames()`
      - `PropertyHookScope.getParameters()`
      - `PropertyHookScope.getOffsetRange()`
      - `PropertyHookElement.isReference()`
      - `PropertyHookElement.hasBody()`
      - `PropertyHookElement.isAttributed()`
      - `PropertyHookElement.getParameters()`
      - `PropertyHookElement.getOffsetRange()`
    
    - Add/Fix unit tests for the index and the model

---
+++ php/php.editor/src/org/netbeans/modules/php/editor/model/impl/PropertyHookSignatureItem.java
@@ -0,0 +1,295 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.netbeans.modules.php.editor.model.impl;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.netbeans.modules.csl.api.OffsetRange;
+import org.netbeans.modules.php.api.util.StringUtils;
+import org.netbeans.modules.php.editor.api.elements.ParameterElement;
+import org.netbeans.modules.php.editor.api.elements.PropertyHookElement;
+import org.netbeans.modules.php.editor.elements.ParameterElementImpl;
+import org.netbeans.modules.php.editor.model.PropertyHookScope;
+
+/**
+ * Represents a JSON format object for a property hook.
+ *
+ * <pre>
+ * e.g.
+ * {
+ *     "name":"set",
+ *     "start":3651,
+ *     "end":3690,
+ *     "mod":1,
+ *     "isRef":false,
+ *     "isAttr":false,
+ *     "hasBody":true,
+ *     "paramSig":"$value::0::1:1:0:0:0:0::"
+ * }
+ * </pre>
+ */
+public class PropertyHookSignatureItem {
+
+    private static final Logger LOGGER = Logger.getLogger(PropertyHookSignatureItem.class.getName());
+    private static final String EMPTY_ARRAY = "[]"; // NOI18N
+
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("start")
+    private int start;
+    @JsonProperty("end")
+    private int end;
+    @JsonProperty("mod")
+    private int mod;
+    @JsonProperty("isRef")
+    private boolean isRef;
+    @JsonProperty("isAttr")
+    private boolean isAttr;
+    @JsonProperty("hasBody")
+    private boolean hasBody;
+    @JsonProperty("paramSig")
+    private String paramSig;
+
+    // need a default constructor for jackson
+    private PropertyHookSignatureItem() {
+    }
+
+    private PropertyHookSignatureItem(String name, int start, int end, int mod, boolean isAttr, boolean isRef, boolean hasBody, String paramSig) {
+        this.name = name;
+        this.start = start;
+        this.end = end;
+        this.mod = mod;
+        this.isAttr = isAttr;
+        this.isRef = isRef;
+        this.hasBody = hasBody;
+        this.paramSig = paramSig;
+    }
+
+    private PropertyHookSignatureItem(PropertyHookScope propertyHook) {
+        this(
+                propertyHook.getName(),
+                propertyHook.getOffsetRange().getStart(),
+                propertyHook.getOffsetRange().getEnd(),
+                propertyHook.getPhpModifiers().toFlags(),
+                propertyHook.isAttributed(),
+                propertyHook.isReference(),
+                propertyHook.hasBody(),
+                getParameterSignature(propertyHook.getParameters())
+        );
+    }
+
+    private PropertyHookSignatureItem(PropertyHookElement element) {
+        this(
+                element.getName(),
+                element.getOffsetRange().getStart(),
+                element.getOffsetRange().getEnd(),
+                element.getPhpModifiers().toFlags(),
+                element.isAttributed(),
+                element.isReference(),
+                element.hasBody(),
+                getParameterSignature(element.getParameters())
+        );
+    }
+
+    private static String getParameterSignature(List<? extends ParameterElement> params) {
+        StringBuilder sb = new StringBuilder();
+        for (ParameterElement param : params) {
+            ParameterElementImpl parameter = (ParameterElementImpl) param;
+            if (sb.length() > 0) {
+                sb.append(',');
+            }
+            sb.append(parameter.getSignature());
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Get a signature(JSON format) for PropetyHookScopes. (Serialize
+     * PropertyHookScopes.)
+     *
+     * e.g.
+     * `[{"name":"set","start":3651,"end":3690,"mod":1,"isRef":false,"isAttr":false,"hasBody":true,"paramSig":"$value::0::1:1:0:0:0:0::"}]`
+     *
+     * @param propertyHookScopes
+     * @return a signature for scopes
+     */
+    public static String getSignatureFromScopes(Collection<? extends PropertyHookScope> propertyHookScopes) {
+        final long start = (LOGGER.isLoggable(Level.FINE)) ? System.currentTimeMillis() : 0;
+        List<PropertyHookSignatureItem> signatureItems = getSignatureItemsFromScopes(propertyHookScopes);
+        String signature = EMPTY_ARRAY;
+        if (!signatureItems.isEmpty()) {
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                signature = mapper.writeValueAsString(signatureItems);
+            } catch (JsonProcessingException ex) {
+                LOGGER.log(Level.WARNING, "Cannot serialize: {0}, {1}", new Object[]{signatureItems, ex.getMessage()}); // NOI18N
+            }
+            if (LOGGER.isLoggable(Level.FINE)) {
+                LOGGER.log(Level.FINE, "getSignatureFromScopes() took: {0} ms", (System.currentTimeMillis() - start)); // NOI18N
+            }
+        }
+        return signature;
+    }
+
+    private static List<PropertyHookSignatureItem> getSignatureItemsFromScopes(Collection<? extends PropertyHookScope> scopes) {
+        List<PropertyHookScope> propertyHooks = new ArrayList<>(scopes);
+        propertyHooks.sort((hook1, hook2) -> Integer.compare(hook1.getOffset(), hook2.getOffset()));
+        List<PropertyHookSignatureItem> signatureItems = new ArrayList<>();
+        for (PropertyHookScope propertyHook : propertyHooks) {
+            signatureItems.add(new PropertyHookSignatureItem(propertyHook));
+        }
+        return signatureItems;
+    }
+
+    /**
+     * Get a signature(JSON format) for PropetyHookElements. (Serialize
+     * PropertyHookElements.)
+     *
+     * e.g.
+     * `[{"name":"set","start":3651,"end":3690,"mod":1,"isRef":false,"isAttr":false,"hasBody":true,"paramSig":"$value::0::1:1:0:0:0:0::"}]`
+     *
+     * @param propertyHookElements
+     * @return a signature for elements
+     */
+    public static String getSignatureFromElements(Collection<? extends PropertyHookElement> propertyHookElements) {
+        final long start = (LOGGER.isLoggable(Level.FINE)) ? System.currentTimeMillis() : 0;
+        List<PropertyHookSignatureItem> signatureItems = getSignatureItemsFromElements(propertyHookElements);
+        String signature = EMPTY_ARRAY;
+        if (!signatureItems.isEmpty()) {
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                signature = mapper.writeValueAsString(signatureItems);
+            } catch (JsonProcessingException ex) {
+                LOGGER.log(Level.WARNING, "Cannot serialize: {0}, {1}", new Object[]{signatureItems, ex.getMessage()}); // NOI18N
+            }
+            if (LOGGER.isLoggable(Level.FINE)) {
+                LOGGER.log(Level.FINE, "getSignatureFromElements() took: {0} ms", (System.currentTimeMillis() - start)); // NOI18N
+            }
+        }
+        return signature;
+    }
+
+    private static List<PropertyHookSignatureItem> getSignatureItemsFromElements(Collection<? extends PropertyHookElement> elements) {
+        List<PropertyHookElement> propertyHooks = new ArrayList<>(elements);
+        propertyHooks.sort((hook1, hook2) -> Integer.compare(hook1.getOffset(), hook2.getOffset()));
+        List<PropertyHookSignatureItem> signatureItems = new ArrayList<>();
+        for (PropertyHookElement propertyHook : propertyHooks) {
+            signatureItems.add(new PropertyHookSignatureItem(propertyHook));
+        }
+        return signatureItems;
+    }
+
+    /**
+     * Get property hook items from a JSON format signature. (Deserialize a
+     * signature)
+     *
+     * e.g.
+     * `[{"name":"set","start":3651,"end":3690,"mod":1,"isRef":false,"isAttr":false,"hasBody":true,"paramSig":"$value::0::1:1:0:0:0:0::"}]`
+     *
+     * @param signature JSON formatted signature
+     * @return signature items
+     */
+    public static List<PropertyHookSignatureItem> fromSignature(final String signature) {
+        if (StringUtils.isEmpty(signature) || EMPTY_ARRAY.equals(signature)) {
+            return List.of();
+        }
+
+        final long start = (LOGGER.isLoggable(Level.FINE)) ? System.currentTimeMillis() : 0;
+        List<PropertyHookSignatureItem> signatureItems = List.of();
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            signatureItems = mapper.readValue(signature, new TypeReference<List<PropertyHookSignatureItem>>() {});
+        } catch (JsonProcessingException ex) {
+            LOGGER.log(Level.WARNING,
+                    "Cannot deserialize: {0}, {1} (please try to delete your cache directory because the signature may be changed.)", // NOI18N
+                    new Object[]{signature, ex.getMessage()});
+        }
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.log(Level.FINE, "fromSignature() took: {0} ms", (System.currentTimeMillis() - start)); // NOI18N
+        }
+        return signatureItems;
+    }
+
+    @JsonIgnore
+    public String getName() {
+        return name;
+    }
+
+    @JsonIgnore
+    public OffsetRange getOffsetRange() {
+        return new OffsetRange(start, end);
+    }
+
+    @JsonIgnore
+    public int getStart() {
+        return start;
+    }
+
+    @JsonIgnore
+    public int getEnd() {
+        return end;
+    }
+
+    @JsonIgnore
+    public int getModifier() {
+        return mod;
+    }
+
+    @JsonIgnore
+    public boolean isAttributed() {
+        return isAttr;
+    }
+
+    @JsonIgnore
+    public boolean isReference() {
+        return isRef;
+    }
+
+    @JsonIgnore
+    public boolean hasBody() {
+        return hasBody;
+    }
+
+    @JsonIgnore
+    public String getParameterSignature() {
+        return paramSig;
+    }
+
+    @Override
+    public String toString() {
+        return "PropertyHookSignatureItem{" // NOI18N
+                + "name=" + name // NOI18N
+                + ", start=" + start // NOI18N
+                + ", end=" + end // NOI18N
+                + ", mod=" + mod // NOI18N
+                + ", isRef=" + isRef // NOI18N
+                + ", isAttr=" + isAttr // NOI18N
+                + ", hasBody=" + hasBody // NOI18N
+                + ", paramSig=" + paramSig // NOI18N
+                + '}';
+    }
+}
